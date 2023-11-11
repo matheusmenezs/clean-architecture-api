d2c3ad28d9050fa4e24d1fcd780530a0f7964d58
Matheus Menezes <matheushm1798@unifei.edu.br>
2023-11-11 14:48:20 -0300

    feat: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,8 @@
 export class SignUpController {
   handle (httpRequest: any): any {
     const httpResponse = {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: name')
     }
 
     return httpResponse
