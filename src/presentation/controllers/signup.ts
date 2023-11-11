efad1b053423c163b745438819df879b656f0d09
Matheus Menezes <matheushm1798@unifei.edu.br>
2023-11-11 15:05:04 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,18 @@
 export class SignUpController {
   handle (httpRequest: any): any {
+    let body = {}
+
+    if (!httpRequest.body.name) {
+      body = new Error('Missing param: name')
+    }
+
+    if (!httpRequest.body.email) {
+      body = new Error('Missing param: email')
+    }
+
     const httpResponse = {
       statusCode: 400,
-      body: new Error('Missing param: name')
+      body
     }
 
     return httpResponse
